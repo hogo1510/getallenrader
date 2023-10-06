2977bad0c92f40eb2a808db5f956089467500317
Lucas Fenstra <55227884+hogo1510@users.noreply.github.com>
2023-10-06 16:21:10 +0200

    basis file is geupload

---
+++ getallenrader.py
@@ -0,0 +1,94 @@
+import random
+def vraag():
+    vraag = str(input("Kies: b(bot) p(player): "))
+
+    hoogte = str(input("hoe hoog mag het getal maximum zijn?: "))
+    pogingQ = input("met een vast gesteld hoeveelheid pogingen? y(yes) n(no=oneindig): ")
+
+    pogingSet = None
+
+    if pogingQ == "y":
+        pogingSet = True
+        hvlpogingen = int(input("Hoeveel pogingen ?: "))
+    elif pogingQ == "n":
+        hvlpogingen = None
+        pogingSet = False
+    else:
+        print(pogingQ +" Word niet herkend, kies opniew")
+
+        #Main Settings above
+        #specific Setting under
+
+    if vraag == "b":
+        print("tegen een bot")
+        HoogteInt = int(hoogte)
+        antw = random.randint(0,HoogteInt)
+        game_bot(antw, pogingSet, hvlpogingen)
+
+    elif vraag == "p":
+        naam = input("kies de naam voor player 2: ")
+        getal = int(input(naam +" kies een getal tussen de 1 en " +hoogte +" :"))
+        game_player(getal, pogingSet, hvlpogingen)
+
+    else:
+        print(vraag +" wordt niet herkend, kies opniew")
+        vraag()
+    
+def game_player(antw, pogingSet, hvlpogingen): #Game Function for against an other Player
+    WStatus = False
+    if pogingSet == True:
+        while hvlpogingen > 0 and WStatus==False:
+            print("nog", hvlpogingen ,"pogingen")
+            vraag = int(input("doe een gok wat het getal is: "))
+            if vraag == antw:
+                print("goed geraden, dit is het juiste antwoord")
+                WStatus = True
+            else:
+                print("fout probeer nog eens")
+                hvlpogingen -= 1
+    else:
+        vraag = None
+        while vraag != antw or vraag == None:
+            vraag = int(input("doe eens een gok wat het getal is: "))
+        else:
+            print("goed geraden, dit is het juiste antwoord!")
+            WStatus = True
+    exit(WStatus)
+
+
+def game_bot(antw, pogingSet, hvlpogingen): #Game Function for agaisnt a bot
+#    print(antw)
+    WStatus = False
+    if pogingSet == True:
+        while hvlpogingen > 0 and WStatus==False:
+            print("nog", hvlpogingen ,"pogingen")
+            vraag = int(input("doe een gok wat het getal is: "))
+            if vraag == antw:
+                print("goed geraden, dit is het juiste antwoord")
+                WStatus = True
+            else:
+                print("fout probeer nog eens")
+                hvlpogingen -= 1
+    else:
+        vraag = None
+        while vraag != antw or vraag == None:
+            vraag = int(input("doe eens een gok wat het getal is: "))
+        else:
+            print("goed geraden, dit is het juiste antwoord!")
+            WStatus = True
+    print(antw)
+    exit(WStatus)
+
+
+
+def exit(WOL):
+    if WOL == True:
+        print("gefeliciteerd !")
+    else:
+        print("Jammer man")
+
+print("welkom bij de getallen rader !")
+vraag()
+
+
+
